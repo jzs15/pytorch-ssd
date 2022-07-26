925209f9c7c613557bf954d08db31bb0f88a1c8b
jzs15 <wlstjr214@naver.com>
2022-07-26 11:11:12 +0900

    오류 부분 shape 확인

--- train_ssd.py
+++ train_ssd.py
@@ -437,86 +437,75 @@ def cal_boxdiff(args, net_state_dict, DEVICE, iou_threshold, label_file, config)
 
     facnt = 0
 
-    try:
-        for i in range(len(dataset)):
-            image = dataset.get_image(i)
-            a, gtbox, gtlabel = dataset.__getitem__(i)
+    for i in range(len(dataset)):
+        image = dataset.get_image(i)
+        a, gtbox, gtlabel = dataset.__getitem__(i)
 
-            currcnt = gtbox.shape[0]
-            currtargetcnt = np.count_nonzero(gtlabel==1)
-            currtextcnt = np.count_nonzero(gtlabel==2)
-            totalcnt = totalcnt + gtbox.shape[0]
-            totaltargetcnt = totaltargetcnt + currtargetcnt
-            totaltextcnt = totaltextcnt + currtextcnt
+        currcnt = gtbox.shape[0]
+        currtargetcnt = np.count_nonzero(gtlabel == 1)
+        currtextcnt = np.count_nonzero(gtlabel == 2)
+        totalcnt = totalcnt + gtbox.shape[0]
+        totaltargetcnt = totaltargetcnt + currtargetcnt
+        totaltextcnt = totaltextcnt + currtextcnt
 
-            gtboxes = torch.tensor(gtbox)
-            print(f'INFO(Bug Fix): CP1')
-            boxes, labels, probs = predictor.predict(image, 30, iou_threshold)
-            sum = 0
-            sumtarget = 0
-            sumtext = 0
-            targetcnt = 0
-            textcnt = 0
+        gtboxes = torch.tensor(gtbox)
+        print(f'INFO(Bug Fix): CP1')
+        boxes, labels, probs = predictor.predict(image, 30, iou_threshold)
+        sum = 0
+        sumtarget = 0
+        sumtext = 0
+        targetcnt = 0
+        textcnt = 0
 
-            predcnt = list(boxes.size())[0]
-            currmatchcnt = 0
-            currmatchtargetcnt = 0
-            currmatchtextcnt = 0
+        predcnt = list(boxes.size())[0]
+        currmatchcnt = 0
+        currmatchtargetcnt = 0
+        currmatchtextcnt = 0
 
-            currfacnt = 0
+        currfacnt = 0
 
-            for j in range(gtboxes.size(0)):
-                iou = box_utils.iou_of(gtboxes[j], boxes)
-                maxval = torch.max(iou)
-                xor = 1 - maxval
-                sum = sum + xor
+        for j in range(gtboxes.size(0)):
+            iou = box_utils.iou_of(gtboxes[j], boxes)
+            maxval = torch.max(iou)
+            xor = 1 - maxval
+            sum = sum + xor
+
+            if gtlabel[j] == 1:
+                sumtarget = sumtarget + xor
+                targetcnt = targetcnt + 1
+            elif gtlabel[j] == 2:
+                sumtext = sumtext + xor
+                textcnt = textcnt + 1
+
+            if maxval > iou_threshold:
+                currmatchcnt = currmatchcnt + 1
 
                 if gtlabel[j] == 1:
-                    sumtarget = sumtarget + xor
-                    targetcnt = targetcnt + 1
+                    currmatchtargetcnt = currmatchtargetcnt + 1
                 elif gtlabel[j] == 2:
-                    sumtext = sumtext + xor
-                    textcnt = textcnt + 1
+                    currmatchtextcnt = currmatchtextcnt + 1
 
-                if maxval > iou_threshold:
-                    currmatchcnt = currmatchcnt + 1
+        totalsum = totalsum + sum / gtboxes.size(0)
+        totalsumtarget = totalsumtarget + sumtarget / targetcnt
+        totalsumtext = totalsumtext + sumtext / textcnt
 
-                    if gtlabel[j] == 1:
-                        currmatchtargetcnt = currmatchtargetcnt + 1
-                    elif gtlabel[j] == 2:
-                        currmatchtextcnt = currmatchtextcnt + 1
+        matchcnt = matchcnt + currmatchcnt
+        matchtargetcnt = matchtargetcnt + currmatchtargetcnt
+        matchtextcnt = matchtextcnt + currmatchtextcnt
 
-            totalsum = totalsum + sum / gtboxes.size(0)
-            totalsumtarget = totalsumtarget + sumtarget / targetcnt
-            totalsumtext = totalsumtext + sumtext / textcnt
+        facheck = list(probs > iou_threshold).count(True) - gtboxes.size(0)
 
-            matchcnt = matchcnt + currmatchcnt
-            matchtargetcnt = matchtargetcnt + currmatchtargetcnt
-            matchtextcnt = matchtextcnt + currmatchtextcnt
+        if facheck > 0:
+            facnt = facnt + facheck
 
-            facheck = list(probs > iou_threshold).count(True) - gtboxes.size(0)
+    retavr = (totalsum / len(dataset)).item()
+    retavrtarget = (totalsumtarget / len(dataset)).item()
+    retavrtext = (totalsumtext / len(dataset)).item()
 
-            if facheck > 0:
-                facnt = facnt + facheck
-
-        retavr = (totalsum/len(dataset)).item()
-        retavrtarget = (totalsumtarget/len(dataset)).item()
-        retavrtext = (totalsumtext/len(dataset)).item()
-
-        rettotalap = matchcnt/totalcnt
-        rettotaltargetap = matchtargetcnt/totaltargetcnt
-        rettotaltextap = matchtextcnt/totaltextcnt
-        retfacnt = facnt
-
-    except:
-        retavr = 1.0
-        retavrtarget = 1.0
-        retavrtext = 1.0
-
-        rettotalap = 0
-        rettotaltargetap = 0
-        rettotaltextap = 0
-        retfacnt = 0
+    rettotalap = matchcnt / totalcnt
+    rettotaltargetap = matchtargetcnt / totaltargetcnt
+    rettotaltextap = matchtextcnt / totaltextcnt
+    retfacnt = facnt
 
     return retavr, retavrtarget, retavrtext, rettotalap, rettotaltargetap, rettotaltextap, retfacnt
 
